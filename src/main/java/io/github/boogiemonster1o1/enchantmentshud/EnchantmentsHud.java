86a770e368407e3635870d40c90efa59113008f8
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-03 22:27:24 +0530

    Mod-ified more files
     Changes to be committed:
    	modified:   README.md
    	new file:   src/main/java/io/github/boogiemonster1o1/enchantmentshud/EnchantmentsHud.java
    	deleted:    src/main/java/net/fabricmc/example/ExampleMod.java
    	deleted:    src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
    	modified:   src/main/resources/fabric.mod.json
    	modified:   src/main/resources/modid.mixins.json

---
+++ src/main/java/io/github/boogiemonster1o1/enchantmentshud/EnchantmentsHud.java
@@ -0,0 +1,10 @@
+package io.github.boogiemonster1o1.enchantmentshud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnchantmentsHud implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
