f9f3ee913c60fe2520a85f2446964c6f835052e7
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-03 23:15:49 +0530

    I think I did it
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/enchantmentshud/mixin/InGameHudMixin.java

--- src/main/java/io/github/boogiemonster1o1/enchantmentshud/mixin/InGameHudMixin.java
+++ src/main/java/io/github/boogiemonster1o1/enchantmentshud/mixin/InGameHudMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.util.Window;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,55 +25,48 @@ public abstract class InGameHudMixin {
     @Shadow private ItemStack heldItem;
     @Shadow private int heldItemTooltipFade;
 
+    private StringBuilder enchantments;
+
     @Inject(method="method_2420",at=@At(value="INVOKE",target = "Lnet/minecraft/item/ItemStack;getName()Ljava/lang/String;"))
     public void enchantments(Window window,CallbackInfo ci){
         if (this.heldItemTooltipFade > 0 && this.heldItem != null && this.heldItem.hasEnchantments()) {
             ListTag enchantmentList = this.heldItem.getEnchantments();
-            System.out.println(enchantmentList.getString(0));
             StringBuilder enchantments = new StringBuilder();
             for(int a = 0; a < enchantmentList.size();a++){
                 String val = enchantmentList.getString(a);
                 val = val.replace("{","");
                 val = val.replace("}","");
                 val = val.replace("s","");
-                String[] arr = new String[2];
-                arr = val.split(",");
+                String[] arr = val.split(",");
                 String[] level = arr[0].split(":");
-                int enchantmentlevel = Integer.parseInt(level[1]);
+                int enchantmentLevel = Integer.parseInt(level[1]);
                 String[] enchantmentId = arr[1].split(":");
                 String enchantmentName = I18n.translate(Enchantment.byRawId(Integer.parseInt(enchantmentId[1])).getTranslationKey());
                 enchantments.append(enchantmentName);
                 enchantments.append(" ");
-                enchantments.append(enchantmentlevel);
+                enchantments.append(enchantmentLevel);
+                enchantments.append("   ");
             }
-
-            System.out.println(enchantments);
-
-//            String string = this.heldItem.getName();
-//            if (this.heldItem.hasCustomName()) {
-//                string = Formatting.ITALIC + string;
-//            }
-//
-//            int i = (window.getScaledWidth() - this.getFontRenderer().getStringWidth(string)) / 2;
-//            int j = window.getScaledHeight() - 59;
-//            if (!this.client.interactionManager.hasStatusBars()) {
-//                j += 14;
-//            }
-//
-//            int k = (int)((float)this.heldItemTooltipFade * 256.0F / 10.0F);
-//            if (k > 255) {
-//                k = 255;
-//            }
-//
-//            if (k > 0) {
-//                GlStateManager.pushMatrix();
-//                GlStateManager.enableBlend();
-//                GlStateManager.blendFuncSeparate(770, 771, 1, 0);
-//                this.getFontRenderer().drawWithShadow(string, (float)i, (float)j, 16777215 + (k << 24));
-//                GlStateManager.disableBlend();
-//                GlStateManager.popMatrix();
-//            }
+            this.enchantments = enchantments;
+            System.out.println(enchantments.toString());
         }
+    }
+
+    @Inject(method="method_2420",at=@At(value="INVOKE",target = "Lcom/mojang/blaze3d/platform/GlStateManager;disableBlend()V"))
+    public void displayEnchantments(Window window,CallbackInfo ci){
+        int ii = (window.getScaledWidth() - this.getFontRenderer().getStringWidth(enchantments.toString())) / 2;
+        int jj = window.getScaledHeight() - 59;
+        if (!this.client.interactionManager.hasStatusBars()) {
+            jj += 14;
+        }
+
+        int kk = (int)((float)this.heldItemTooltipFade * 256.0F / 10.0F);
+        if (kk > 255) {
+            kk = 255;
+        }
+        String finalText = Formatting.BLUE + enchantments.toString() + Formatting.RESET;
+        System.out.println(finalText);
+        this.getFontRenderer().drawWithShadow(finalText, (float)ii, (float)jj + 5, 16777215 + (kk << 24));
     }
 
     @Shadow
